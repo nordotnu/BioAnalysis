092e20ae180fa3684e70609b5d7a1e51ca05a069
nm222ra <nm222ra@student.lnu.se>
2024-04-17 22:03:12 +0200

    Added Plotting

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include "../lib/imgui/imgui.h"
 #include "../lib/imgui/imgui_impl_glfw.h"
 #include "../lib/imgui/imgui_impl_opengl3.h"
+#include "implot.h"
+#include "math.h"
+#include "string"
 #include <GLFW/glfw3.h> // Will drag system OpenGL headers
 
 static void glfw_error_callback(int error, const char *description)
@@ -9,13 +12,88 @@ static void glfw_error_callback(int error, const char *description)
   fprintf(stderr, "GLFW Error %d: %s\n", error, description);
 }
 
+// utility structure for realtime plot
+struct RollingBuffer
+{
+  float Span;
+  ImVector<ImVec2> Data;
+  RollingBuffer()
+  {
+    Span = 10.0f;
+    Data.reserve(2000);
+  }
+  void AddPoint(float x, float y)
+  {
+    float xmod = fmodf(x, Span);
+    if (!Data.empty() && xmod < Data.back().x)
+      Data.shrink(0);
+    Data.push_back(ImVec2(xmod, y));
+  }
+};
+
+void RealtimePlots(float valueA, float valueB, float valueC, float valueD)
+{
+
+  static RollingBuffer va, vb, vc, vd;
+  static float t = 0;
+  t += ImGui::GetIO().DeltaTime;
+  va.AddPoint(t, valueA);
+  vb.AddPoint(t, valueB);
+  vc.AddPoint(t, valueC);
+  vd.AddPoint(t, valueD);
+
+  static ImPlotAxisFlags flags = ImPlotAxisFlags_NoTickLabels;
+
+  if (ImPlot::BeginPlot("sEMG", ImVec2(-1, 400)))
+  {
+    ImPlot::SetupAxes(nullptr, nullptr, flags, flags);
+    ImPlot::SetupAxisLimits(ImAxis_X1, 0, 10, ImGuiCond_Always);
+    ImPlot::SetupAxisLimits(ImAxis_Y1, -100, 3500);
+    ImPlot::PlotLine("A", &va.Data[0].x, &va.Data[0].y, va.Data.size(), 0, 0, 2 * sizeof(float));
+    ImPlot::PlotLine("B", &vb.Data[0].x, &vb.Data[0].y, vb.Data.size(), 0, 0, 2 * sizeof(float));
+    ImPlot::PlotLine("C", &vc.Data[0].x, &vc.Data[0].y, vc.Data.size(), 0, 0, 2 * sizeof(float));
+    ImPlot::PlotLine("D", &vd.Data[0].x, &vd.Data[0].y, vd.Data.size(), 0, 0, 2 * sizeof(float));
+    ImPlot::EndPlot();
+  }
+}
+
 int main()
 {
+
+  // Serial data receiver.
+  int status = 0;
+  SerialDataReceiver sdr("/dev/ttyUSB0", 123, &status);
+  sdr.openPort();
+  std::thread thread_obj(&SerialDataReceiver::receiveData, &sdr);
+  if (status != 0)
+  {
+    return -1;
+  }
+
+  /*
+    auto startTime = std::chrono::high_resolution_clock::now();
+    while (status == 0)
+    {
+      auto currentTime = std::chrono::high_resolution_clock::now();
+      auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - startTime);
+      if (elapsedTime.count() >= 2)
+      {
+        startTime = currentTime;
+        sdr.m.lock();
+        if (sdr.data.size() == 4)
+        {
+          printf("A: %04u, B: %4u, C: %04u, D: %04u (R=%d), ERROR=%d, status=%d\n", sdr.data.at(0), sdr.data.at(1), sdr.data.at(2), sdr.data.at(3), sdr.getSampleRate(), sdr.getErrorCount(), status);
+          std::cout << "\033[1A"
+                    << "\033[K";
+        }
+        sdr.m.unlock();
+      }
+    }*/
+
   // Imgui
   glfwSetErrorCallback(glfw_error_callback);
   if (!glfwInit())
     return 1;
-
 
   const char *glsl_version = "#version 130";
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -26,24 +104,24 @@ int main()
     return 1;
   glfwMakeContextCurrent(window);
   glfwSwapInterval(1); // Enable vsync
-
   ImGui::CreateContext();
+
+  ImPlot::CreateContext();
   ImGuiIO &io = ImGui::GetIO();
   (void)io;
   io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
   io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;  // Enable Gamepad Controls
-  io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;     // Enable Docking
-  io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;   // Enable Multi-Viewport / Platform Windows
-
-
+                                                        // io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;     // Enable Docking
+  // io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;   // Enable Multi-Viewport / Platform Windows
+  // io.Fonts->AddFontFromFileTTF("inter.ttf", 20.0f);
   // Setup Dear ImGui style
-  ImGui::StyleColorsDark();
+  ImGui::StyleColorsClassic();
 
   // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
   ImGuiStyle &style = ImGui::GetStyle();
   if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
   {
-    style.WindowRounding = 0.0f;
+    style.WindowRounding = 4.5f;
     style.Colors[ImGuiCol_WindowBg].w = 1.0f;
   }
 
@@ -51,34 +129,38 @@ int main()
   ImGui_ImplGlfw_InitForOpenGL(window, true);
   ImGui_ImplOpenGL3_Init(glsl_version);
 
-  ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+  ImVec4 clear_color = ImVec4(0.0f, 0.0f, 0.0f, 1.00f);
 
   while (!glfwWindowShouldClose(window))
   {
-    // Poll and handle events (inputs, window resize, etc.)
-    // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-    // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
-    // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
-    // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
     glfwPollEvents();
 
     // Start the Dear ImGui frame
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
-
-
-    // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+    float va = 0.0f;
+    float vb = 0.0f;
+    float vc = 0.0f;
+    float vd = 0.0f;
+    if (status == 0)
     {
-      static float f = 0.0f;
-      static int counter = 0;
-
-      ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!" and append into it.
-
-
-      ImGui::End();
+      sdr.m.lock();
+      if (sdr.data.size() == 4)
+      {
+        va = (float)sdr.data.at(0);
+        vb = (float)sdr.data.at(1);
+        vc = (float)sdr.data.at(2);
+        vd = (float)sdr.data.at(3);
+      }
+      sdr.m.unlock();
     }
 
+    ImGui::Begin("My Window", nullptr, ImGuiWindowFlags_NoTitleBar);
+    Demo_RealtimePlots(va, vb, vc, vd);
+    ImGui::Text("Sampling Rate: %d Hz", sdr.getSampleRate());
+
+    ImGui::End();
 
     // Rendering
     ImGui::Render();
@@ -106,35 +188,11 @@ int main()
   // Cleanup
   ImGui_ImplOpenGL3_Shutdown();
   ImGui_ImplGlfw_Shutdown();
+  ImPlot::DestroyContext();
   ImGui::DestroyContext();
 
   glfwDestroyWindow(window);
   glfwTerminate();
 
-
-  // Serial data receiver.
-  int status = 0;
-  SerialDataReceiver sdr("/dev/ttyUSB0", 123, &status);
-  sdr.openPort();
-  std::thread thread_obj(&SerialDataReceiver::receiveData, &sdr);
-
-  auto startTime = std::chrono::high_resolution_clock::now();
-  while (status == 0)
-  {
-    auto currentTime = std::chrono::high_resolution_clock::now();
-    auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - startTime);
-    if (elapsedTime.count() >= 2)
-    {
-      startTime = currentTime;
-      sdr.m.lock();
-      if (sdr.data.size() == 4)
-      {
-        printf("A: %04u, B: %4u, C: %04u, D: %04u (R=%d), ERROR=%d, status=%d\n", sdr.data.at(0), sdr.data.at(1), sdr.data.at(2), sdr.data.at(3), sdr.getSampleRate(), sdr.getErrorCount(), status);
-        std::cout << "\033[1A"
-                  << "\033[K";
-      }
-      sdr.m.unlock();
-    }
-  }
   sdr.closePort();
 }
